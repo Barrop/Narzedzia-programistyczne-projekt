fe10798cd99bdcca59b058f3761c6fff1cf8c1e5
Marianpol <marcin.musial20@gmail.com>
2018-05-11 13:20:25 +0200

    Comments removal

--- home.js
+++ home.js
@@ -1,18 +1,12 @@
-// When the user scrolls the page, execute myFunction 
 window.onscroll = () => {
 	stickyWindow();
 	}
-
-// Get the navbar
 const menu = document.getElementById("menu");
-
-// Get the offset position of the navbar
 let sticky = menu.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyWindow() {
   if (window.pageYOffset >= sticky) {
-    menu.classList.add("sticky")
+    menu.classList.add("sticky");
   } 
   else {
     menu.classList.remove("sticky");
